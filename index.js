35e22b288e62f00f825b830b9d4494a0697bb569
Sarah Bunker <sarah@sphinxdefense.com>
2023-10-07 09:43:50 -0600

    adding nodeman, adding get single resource

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ let notes = [
     id: 3,
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true
+  },
+  {
+    id: 4,
+    content: "HTTP protocol",
+    important: true
   }
 ]
 
@@ -25,6 +30,16 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
